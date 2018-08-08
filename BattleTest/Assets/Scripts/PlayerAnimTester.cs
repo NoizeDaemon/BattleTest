b1676862f67b69c945fc91b9c862389501f94ac7
Dario Frei <freidario@gmx.at>
2018-08-08 12:58:55 +0200

    Test Sprite added
    Played around with conditional animations

---
+++ BattleTest/Assets/Scripts/PlayerAnimTester.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimTester : MonoBehaviour
+{
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("q"))
+	    {
+	        anim.SetBool("directionUp", true);
+	        anim.SetBool("directionLeft", true);
+        }
+	    else if (Input.GetKeyDown("a"))
+	    {
+	        anim.SetBool("directionUp", false);
+	        anim.SetBool("directionLeft", true);
+	    }
+	    else if (Input.GetKeyDown("w"))
+	    {
+	        anim.SetBool("directionUp", true);
+	        anim.SetBool("directionLeft", false);
+	    }
+	    else if (Input.GetKeyDown("s"))
+	    {
+	        anim.SetBool("directionUp", false);
+	        anim.SetBool("directionLeft", false);
+	    }
+        if (Input.anyKey) anim.SetBool("isWalking", true);
+        else anim.SetBool("isWalking", false);
+    }
+}
